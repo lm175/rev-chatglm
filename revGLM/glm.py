eea8f220b0f5950dd367ed6e1bfe4a380b7e2512
lm12345 <1756000830@qq.com>
2024-09-15 20:48:02 +0800

    Update glm.py

--- revGLM/glm.py
+++ revGLM/glm.py
@@ -7,6 +7,7 @@ from .entity import ChatResponse
 class ChatBot:
 
     base_api = "https://chatglm.cn/chatglm"
+    assistant_id = "65940acff94777010aa6b796"
     refresh_token: str
 
     def __init__(self, Cookie: str) -> None:
@@ -27,7 +28,6 @@ class ChatBot:
 
     @property
     def access_token(self) -> str:
-        """每隔1小时刷新验证令牌"""
         current_time = time.time()
         if not self._access_token or current_time > self._token_expiry:
             refresh_url = f"{self.base_api}/user-api/user/refresh"
@@ -36,11 +36,11 @@ class ChatBot:
                 'Authorization': f'Bearer {self.refresh_token}',
             }
             response = requests.post(refresh_url, headers=headers)
-            if response.status_code == 200:  
+            if response.status_code == 200:
                 self._access_token = f"Bearer {response.json().get('result').get('access_token')}"
-                self._token_expiry = current_time + 3600  
-            else:  
-                raise Exception("Failed to refresh access token")  
+                self._token_expiry = current_time + 1800    # 间隔半小时以上时刷新验证令牌
+            else:
+                raise Exception("Failed to refresh access token")
 
         return self._access_token
 
@@ -72,18 +72,18 @@ class ChatBot:
             prompt: str,
             conversation_id: str = "",
             timeout: int = 60,
-            image: bytes = None
+            images: list[bytes] = None
     ) -> Generator[ChatResponse, None, None]:
-        """流式回复
 
-        Args:
-            prompt (str): 提问内容
-            conversation_id (str, optional): 对话id. Defaults to "".
-            timeout (int, optional): 超时时间. Defaults to 60.
-            image (bytes, optional): 图片二进制数据. Defaults to None.
-        """
+        content = [{"type": "text", "text": prompt}]
+        if images:
+            images_data = []
+            for img in images:
+                images_data.append(self._upload_image(img))
+            content.append({"type": "image", "image": images_data})
+            
         data = {
-            "assistant_id": "65940acff94777010aa6b796", # chatglm
+            "assistant_id": self.assistant_id, # chatglm
             "conversation_id": conversation_id,
             "meta_data": {
                 "mention_conversation_id": "",
@@ -97,12 +97,7 @@ class ChatBot:
             "messages": [
                 {
                     "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": prompt
-                        }
-                    ]
+                    "content": content
                 }
             ]
         }
@@ -134,24 +129,16 @@ class ChatBot:
             prompt: str,
             conversation_id: str = "",
             timeout: int = 60,
-            image: bytes = None
+            images: list[bytes] = None
     ) -> ChatResponse:
-        """非流式回复
-
-        Args:
-            prompt (str): 提问内容
-            conversation_id (str, optional): 对话id. Defaults to "".
-            timeout (int, optional): 超时时间. Defaults to 60.
-            image (bytes, optional): 图片二进制数据. Defaults to None.
-        """
-
+        
         result = None
         image_url = None
         for resp in self._stream_ask(
                 prompt,
                 conversation_id,
                 timeout,
-                image
+                images
         ):
             result = resp
             if result.content_type == "image" :
@@ -167,7 +154,7 @@ class ChatBot:
             conversation_id: str = "",
             timeout: int = 60,
             stream: bool = False,
-            image: bytes = None
+            images: list[bytes] = None
     ) -> Union[Generator[ChatResponse, None, None], ChatResponse]:
         """提问
 
@@ -176,7 +163,7 @@ class ChatBot:
             conversation_id (str, optional): 对话id. Defaults to "".
             timeout (int, optional): 超时时间. Defaults to 60.
             stream (bool, optional): 是否流式. Defaults to False.
-            image (bytes, optional): 图片二进制数据. Defaults to None.
+            images (bytes, optional): 图片二进制数据列表. Defaults to None.
         """
 
         if stream:
@@ -184,13 +171,71 @@ class ChatBot:
                 prompt,
                 conversation_id,
                 timeout,
-                image
+                images
             )
         else:
             return self._non_stream_ask(
                 prompt,
                 conversation_id,
                 timeout,
-                image
+                images
             )
     
+
+    def get_conversations(self) -> list:
+        params = {
+            "assistant_id": self.assistant_id,
+            "page": 1,
+            "page_size": 25
+        }
+        resp = requests.get(
+            url=f"{self.base_api}/backend-api/assistant/conversation/list",
+            params=params,
+            headers=self.headers
+        )
+        return resp.json().get('result').get('conversation_list')
+    
+
+    def del_conversation(self, conversation_id: str) -> bool:
+        """删除指定对话，参数为对话的conversation_id"""
+        data = {
+            "assistant_id": self.assistant_id,
+            "conversation_id": conversation_id
+        }
+        resp = requests.post(f"{self.base_api}/backend-api/assistant/conversation/delete", data=data)
+        if resp.status_code == 200:
+            return True
+        else:
+            return False
+        
+
+    def _upload_image(self, image: bytes) -> dict:
+        headers = {
+            "Accept": "application/json, text/plain, */*",  
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",  
+            "App-Name": "chatglm",  
+            "Authorization": self.access_token,  
+            "Connection": "keep-alive",  
+            "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryMUeqfULMYn0q11JZ",
+            "Origin": "https://chatglm.cn",  
+            "User-Agent": UserAgent().chrome,  
+            "X-App-Platform": "pc",  
+            "X-App-Version": "0.0.1",  
+            "X-Lang": "zh",  
+        }
+
+        filename = str(time.time()).replace('.', '')
+        boundary = '----WebKitFormBoundaryMUeqfULMYn0q11JZ' # 构造文件表单数据
+        data = (
+            b'--' + boundary.encode('latin-1') + b'\r\n'
+            b'Content-Disposition: form-data; name="file"; '
+            b'filename="' + filename.encode('latin-1') + b'"\r\n'
+            b'Content-Type: image/png\r\n\r\n'
+            + image
+            + b'\r\n'
+            b'--' + boundary.encode('latin-1') + b'--\r\n'
+        )
+        response = requests.post(f"{self.base_api}/backend-api/assistant/file_upload", headers=headers, data=data)
+        image_data = response.json().get('result')
+        image_data['image_url'] = image_data.get('file_url')
+        return image_data
